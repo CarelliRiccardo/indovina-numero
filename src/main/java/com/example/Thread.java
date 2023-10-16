cf8abc90452d1cb3c6ec28d3bb74da6fc070894d
lai2-24 <lai2-24@itismeucci.com>
2023-10-16 11:32:04 +0200

    primo commit server multicast

--- src/main/java/com/example/Thread.java
+++ src/main/java/com/example/Thread.java
@@ -3,33 +3,20 @@ package com.example;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
-import java.net.ServerSocket;
 import java.net.Socket;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
+public class Thread {
+    Socket s;
+    public Thread(Socket s)
     {
-         try{
-            
-            while(true){
-                
-                System.out.println("server avviato");
-            ServerSocket server=new ServerSocket(3000);//crea il server
-    
-            Socket s=server.accept();//accetta la richiesta di collegamento del client
-            System.out.println("un client si è connesso");
-
-            Thread t=new Thread(s);
-            }
-            
-            
-    
-            /*BufferedReader in=new BufferedReader(new InputStreamReader(s.getInputStream()));//crea il modo per ascoltare
+        this.s=s;
+    }
+    public void run()
+    {
+        try
+        {
+            int rand=(int) (Math.random()*100)+1;
+            BufferedReader in=new BufferedReader(new InputStreamReader(s.getInputStream()));//crea il modo per ascoltare
             DataOutputStream out= new DataOutputStream(s.getOutputStream());//crea il modo per rispondere
             String stringaRicevuta=in.readLine();//riceve il messaggio dal client
             int num=0;
@@ -56,13 +43,15 @@ public class App
 
             }while(num!=rand);
             System.out.println("4");
-            out.writeBytes("4"+"\n");*/
+            out.writeBytes("4"+"\n");
         }
-        catch(Exception e)
+        catch (Exception e) 
         {
             System.out.println(e.getMessage());
             System.out.println("errore nella creazione del server");
             System.exit(1);
         }
+ 
     }
+    
 }
